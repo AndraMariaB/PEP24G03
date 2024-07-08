d990fb14f1dc9f6de330da50a7491c09c65cb331
emcraciu <ozy24us@gmail.com>
2024-07-08 20:41:42 +0300

    modul1

--- modul1/part1.py
+++ modul1/part1.py
@@ -7,8 +7,15 @@ print("text1", 'text2', sep="###")
 # print a triangle
 
 """
- /\ 
-/  \
+ /\\ 
+/  \\
 ----
 """
 
+print("   /\    ")
+print(r"  /  \   ")
+print(" /    \\  ")
+print(" ▔▔▔▔▔▔ ")
+
+# print(" ▔▔▔▔▔▔ " # syntax error
+# print(test) # Exception - semantic error
