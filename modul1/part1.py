bb51c3646c76ab83291f5ea6bd2ca3d9dc8e4a56
emcraciu <ozy24us@gmail.com>
2024-07-08 21:24:56 +0300

    modul1

--- modul1/part1.py
+++ modul1/part1.py
@@ -19,3 +19,21 @@ print(" ▔▔▔▔▔▔ ")
 
 # print(" ▔▔▔▔▔▔ " # syntax error
 # print(test) # Exception - semantic error
+
+# Variable
+
+my_var1 = "Hello World"
+my_var2 = 10
+print(my_var1, my_var2)
+print(type(my_var1))
+result = type(my_var2)
+print(result)
+my_var3 = "experiment"
+
+result2 = print(my_var3)
+print(result2)
+print(type(result2))
+
+result3 = "None"
+print(result3)
+print(type(result3))
