56a159321e4807c4eaabbd791ee9370a7e932864
emcraciu <ozy24us@gmail.com>
2024-07-10 21:15:20 +0300

    modul2

--- modul2/app3.py
+++ modul2/app3.py
@@ -1,4 +1,6 @@
 number_one = int(input("Give first number"))
 number_two = int(input("Give second number"))
 average = (number_one + number_two) / 2
-print(average)
+division = number_one / number_two
+power = number_one ** number_two
+print("media nr este:", average, "\nimpartirea nr este:", int(division), "\na la puterea lui b:", power)
