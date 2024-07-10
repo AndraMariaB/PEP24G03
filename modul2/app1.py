d550394f0621befba43eeeae5c6efbd6ea8690c0
emcraciu <ozy24us@gmail.com>
2024-07-10 19:55:46 +0300

    modu2

---
+++ modul2/app1.py
@@ -0,0 +1,7 @@
+baza = input("introduceti baza: ")
+inaltimea = input("introduceti inaltimea: ")
+baza = int(baza)
+inaltimea = int(inaltimea)
+aria = (baza * inaltimea) / 2
+print(type(aria))
+print(aria)
