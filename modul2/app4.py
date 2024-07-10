194946aebc60acf3028ec74bc9c44878a7092e2a
emcraciu <ozy24us@gmail.com>
2024-07-10 21:32:32 +0300

    modul2

---
+++ modul2/app4.py
@@ -0,0 +1,15 @@
+# venit = int(input("Venit: "))
+# print("Recomandarile noastre:\nCheltuieli uzuale: ", 0.5 * venit,
+#       "\nRecreere: ", 0.3 * venit,
+#       "\nEconomii si datorii: ", 0.2 * venit
+#       )
+
+venit = float(input("Please enter your salary: "))
+print("Venit: ", venit)
+print("Recomandarile noastre: ")
+cheltuieli_usuale = (50 / 100) * venit
+print("Cheltuieli usuale: ", cheltuieli_usuale)
+recreere = (30 / 100) * venit
+print("Recreere: ", recreere)
+economii_datori = (20 / 100) * venit
+print("Economii: ", economii_datori)
