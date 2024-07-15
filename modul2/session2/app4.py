58c8c6283a1775ded00407da21c413fa0a331ccb
emcraciu <ozy24us@gmail.com>
2024-07-15 20:49:48 +0300

    modul2

--- modul2/session2/app4.py
+++ modul2/session2/app4.py
@@ -9,3 +9,20 @@ rejoin3 = "_".join(string3.split())
 print(rejoin1)
 print(rejoin2)
 print(rejoin3)
+
+string1 = string1 + "."
+string2 = string2 + "."
+string3 = string3 + "."
+
+print(string1)
+print(string2)
+print(string3)
+
+words1 = string1.split()
+words2 = string2.split()
+words3 = string3.split()
+
+print(4 * (words1[0] + " ") + ' '. join(words1[1:]))
+print(4 * (words2[0] + " ") + ' '. join(words2[1:]))
+print(4 * (words3[0] + " ") + ' '. join(words3[1:]))
+
