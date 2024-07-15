94be50d4c2cd17e98f0791b5ba9d1edb07946c7d
emcraciu <ozy24us@gmail.com>
2024-07-15 19:54:10 +0300

    modul2

---
+++ modul2/session2/app2.py
@@ -0,0 +1,14 @@
+print("/-\\ ".center(9))
+print("// _ \\\\ ".center(9))
+print("-------".center(9))
+print("\\\\ _ // ".center(9))
+print("\\-/ ".center(9))
+
+print((4 * '-').center(9," "))
+print(('/ \\').center(9," "))
+print(('/_______\\').center(9," "))
+
+print((1 * '*').center(9, " "))
+print((3 * '*').center(9, " "))
+print((5 * '*').center(9, " "))
+print((7 * '*').center(9, " "))
