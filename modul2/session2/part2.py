539fee19be8a2f123c84cd0df6c13734cdc98166
emcraciu <ozy24us@gmail.com>
2024-07-15 21:07:12 +0300

    modul2

--- modul2/session2/part2.py
+++ modul2/session2/part2.py
@@ -14,3 +14,21 @@ print(rejoin)
 
 rejoin_string = " ! ".join('''Hello World''')
 print(rejoin_string)
+
+# object id
+
+var1 = 1
+print(id(var1))
+
+var2 = 3_000_000  # try with 30
+print(id(var2))
+
+var3 = 3_000_001  # try with 31
+print(id(var3))
+
+print("Some random code executed")
+
+result = var1 + var2
+print(id(result))
+
+print("Numbers are equal: ", result == var3)
