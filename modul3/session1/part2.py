ef28dd7619fdd2a134c045c7e0c3c26d28446ce0
emcraciu <ozy24us@gmail.com>
2024-07-22 19:49:09 +0300

    modul3

---
+++ modul3/session1/part2.py
@@ -0,0 +1,15 @@
+# for loops
+
+for letter in "my text":
+    print(letter)
+
+for number in range(2, 7, 1):
+    print(number)
+
+word_list = "my word list".split()
+
+for word in word_list:
+    print(word)
+
+# for number in 100:
+#     print(number)
