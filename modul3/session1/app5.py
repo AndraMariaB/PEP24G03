142ee57f83e36dd79f684bfba95ae2b8ecf16f66
emcraciu <ozy24us@gmail.com>
2024-07-22 21:24:02 +0300

    modul3

--- modul3/session1/app5.py
+++ modul3/session1/app5.py
@@ -1,2 +1,3 @@
-obiecte = ["Masa", 5, "Scaun", 4.5, [5,6,7],8]
-
+objects = ["Masa", 5, "Scaun", 4.5, [5, 6, 7], 8]
+for obj in objects:
+    print("Tipul obiectului", obj, "este", str(type(obj)).split("'")[1])
