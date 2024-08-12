052c7e8f34b5ef80d985c0e3503da22357d8287a
emcraciu <ozy24us@gmail.com>
2024-08-12 18:45:32 +0300

    fix PEP8

--- Homework/Sergiu/tema3.py
+++ Homework/Sergiu/tema3.py
@@ -1,31 +1,30 @@
-#1.
+# 1.
 ##parola = input('introduceti parola: ')
 ##print('Parola introdusa este: ' , parola == 'Passme1n' )
 
-#2.
+# 2.
 
 nume1 = input("Introduceti un nume: ")
 nume2 = input("Introduceti al doilea nume: ")
-print("Lungimea primului nume este de",nume1.__len__() ,"caractere")
-print(nume1==nume2)
-print(nume1.__len__()>nume2.__len__())
-print("Initiala primului nume este: ",nume1[0])
-print("Primul nume inversat: ",nume1[::-1])
+print("Lungimea primului nume este de", nume1.__len__(), "caractere")
+print(nume1 == nume2)
+print(nume1.__len__() > nume2.__len__())
+print("Initiala primului nume este: ", nume1[0])
+print("Primul nume inversat: ", nume1[::-1])
 
-#3.
+# 3.
 
 nr1 = input("Introduceti un numar: ")
 print(nume1 * int(nr1))
 
-#4.
+# 4.
 sir1 = input("introduceti un sir: ")
 print('\n'.join(sir1))
 
-
-#5.
+# 5.
 cuvant = input("Scrieti un cuvant: ")
-print("Palindrom: ",cuvant==cuvant[::-1])
+print("Palindrom: ", cuvant == cuvant[::-1])
 
-#6.
+# 6.
 cuvant2 = input("introduceti un cuvant: ")
-print(cuvant2[0]==cuvant2[0].upper())
+print(cuvant2[0] == cuvant2[0].upper())
