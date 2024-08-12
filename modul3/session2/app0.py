934de037dd8ac74175968a557daa3158e1d1b429
emcraciu <ozy24us@gmail.com>
2024-08-12 20:51:10 +0300

    m3.2

--- modul3/session2/app0.py
+++ modul3/session2/app0.py
@@ -1,1 +1,12 @@
-# GET 2 numbers from user and calculate area in a function 
+# GET 2 numbers from user and calculate area in a function
+
+def calculate_area():
+    print("Calculate area for rectangle")
+    length = int(input("provide length: "))
+    width = int(input("provide width: "))
+    area = length * width
+    # print(area)
+    return area
+
+result = calculate_area()
+print(result)
---
+++ modul3/session2/app1.py
@@ -0,0 +1,39 @@
+def power_of_number(numbers: list):
+    powers = []
+    second_numer = int(input("Provide power of number: "))
+    for number in numbers:
+        result = number ** second_numer
+        powers.append(result)
+    return powers
+
+
+def calculate_numbers():
+    good_numbers = list()
+    user_input = input("Please enter numbers separated by commas: ")
+    user_input_list = user_input.split(',')
+    print(user_input_list)
+    for number in user_input_list:  # type: str
+        if not number.isnumeric():
+            print(f"Number {number} is not a good number. ")
+            continue
+        else:
+            int_number = int(number)
+        if 0 <= int_number <= 100:
+            good_numbers.append(int_number)
+    print("""
+1. Fiecare numar la puterea y
+2. Suma tuturor numerelor din lista
+3. Inmultirea fiecarui numar cu y
+4. Iesire
+    """)
+    option = input("Alegeti optiunea: ").strip()
+    if option == "1":
+        result = power_of_number(good_numbers)
+    else:
+        result = None
+    print(result)
+
+    # print(good_numbers)
+
+
+calculate_numbers()
