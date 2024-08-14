bb0835736eba2e71a78cc6d44bd7256716fe311e
emcraciu <ozy24us@gmail.com>
2024-08-14 20:48:19 +0300

    m4.1

---
+++ modul4/session1/app4.py
@@ -0,0 +1,31 @@
+angajat1 = {
+    'nume': 'Ana-Maria Popescu',
+    'departament': 'IT',
+    'ID': 3409,
+    'Salar': 4560,
+}
+angajat2 = {
+    'nume': 'Marian Muntean',
+    'departament': 'IT',
+    'ID': 2235,
+    'Salar': 4556,
+}
+angajat3 = {
+    'nume': 'Maria Manea',
+    'departament': 'HR',
+    'ID': 1908,
+    'Salar': 6755,
+}
+angajat4 = {
+    'nume': 'Oana Popa',
+    'departament': 'HR',
+    'ID': 1977,
+    'Salar': 5400,
+}
+angajat5 = {
+    'nume': 'David Codru',
+    'departament': 'Management',
+    'ID': 1988,
+    'Salar': 12900,
+}
+lista_dict = [angajat1, angajat2, angajat3, angajat4, angajat5]
